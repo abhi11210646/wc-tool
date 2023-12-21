3b367ed11233c5147bcd14c134167b7c4d720a49
Abhisekh kumar singh <abhisekhs@one.com>
2023-12-21 17:58:26 +0530

    extract readinf file logic to separate func

--- cmd/helper.go
+++ cmd/helper.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func outputHelp() {
 	fmt.Println(`
@@ -13,4 +16,32 @@ func outputHelp() {
 		wc -w logs.txt
 		wc -l logs.txt logs2.txt
 	  `)
+}
+
+func (wc *WC) loadArgs() {
+	args := os.Args[1:]
+	for i, a := range args {
+		if a == "-h" {
+			outputHelp()
+			return
+		}
+		switch a {
+		case "-l":
+			wc.lineFlag = true
+			wc.flagCount++
+		case "-w":
+			wc.wordFlag = true
+			wc.flagCount++
+		case "-m":
+			wc.charFlag = true
+			wc.flagCount++
+		default:
+			wc.filePath = append(wc.filePath, args[i])
+		}
+	}
+	if wc.flagCount == 0 { // if no flag then display all
+		wc.lineFlag = true
+		wc.wordFlag = true
+		wc.charFlag = true
+	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -3,74 +3,74 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
-type WC struct {
-	lineFlag bool
-	wordFlag bool
-	charFlag bool
-	filePath []string
+type Stats struct {
+	lines int
+	words int
+	chars int
 }
 
-func (wc *WC) loadArgs() {
-	args := os.Args[1:]
-	for i, a := range args {
-		if a == "-h" {
-			outputHelp()
-			return
+type WC struct {
+	lineFlag  bool
+	wordFlag  bool
+	charFlag  bool
+	flagCount int
+	filePath  []string
+	stats     []Stats
+}
+
+func (wc *WC) fileStats(reader io.Reader) Stats {
+	scanner := bufio.NewScanner(reader)
+	stats := Stats{lines: -1} // (No. of lines - 1) = newline counts
+	for scanner.Scan() {
+		text := scanner.Text()
+		if wc.charFlag {
+			stats.chars += len(text)
 		}
-		switch a {
-		case "-l":
-			wc.lineFlag = true
-		case "-w":
-			wc.wordFlag = true
-		case "-m":
-			wc.charFlag = true
-		default:
-			wc.filePath = append(wc.filePath, args[i])
+		if wc.wordFlag {
+			stats.words += len(strings.Fields(text))
+		}
+		if wc.lineFlag {
+			stats.lines++
 		}
 	}
+	wc.stats = append(wc.stats, stats)
+	if wc.lineFlag {
+		fmt.Printf("%2d", stats.lines)
+	}
+	if wc.wordFlag {
+		fmt.Printf("%2d", stats.words)
+	}
+	if wc.charFlag {
+		fmt.Printf("%2d", stats.chars)
+	}
+	return stats
 }
 
 func main() {
-	wc := WC{}
+	wc := WC{stats: []Stats{}}
 	wc.loadArgs()
 
 	if len(wc.filePath) > 0 {
-		total := 0
-		for _, file := range wc.filePath {
-			reader, _ := os.Open(file)
-			defer reader.Close()
-			scanner := bufio.NewScanner(reader)
-			l := 0
-			w := 0
-			c := 0
-			for scanner.Scan() {
-				text := scanner.Text()
-				c += len(text)
-				w += len(strings.Fields(text))
-				l++
+		// total := 0
+		for i, file := range wc.filePath {
+			reader, err := os.Open(file)
+			if err != nil {
+				panic(err)
 			}
-			total += l
-			fmt.Println(l, w, c, file)
+			defer reader.Close()
+			wc.fileStats(reader)
+			fmt.Printf(" %s\n", wc.filePath[i])
 		}
-		if len(wc.filePath) > 1 {
-			fmt.Println(total, "total")
-		}
+		// if len(wc.stats) > 1 {
+		// fmt.Printf("\t%s\t%s", total, "total")
+		// }
 	} else {
-		r := bufio.NewReader(os.Stdin)
-		scanner := bufio.NewScanner(r)
-		l := 0
-		w := 0
-		c := 0
-		for scanner.Scan() {
-			text := scanner.Text()
-			c += len(text)
-			w += len(strings.Fields(text))
-			l++
-		}
-		fmt.Println(l, w, c)
+		reader := bufio.NewReader(os.Stdin)
+		wc.fileStats(reader)
 	}
 }
